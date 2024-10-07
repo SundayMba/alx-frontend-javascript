8b184954c64826f8d8a9d676fd0590fa6d68ce36
SundayMba <sundinoh@gmail.com>
2024-10-07 08:19:43 +0100

    fix: fix linter error

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -13,19 +13,3 @@ export function taskNext() {
 
   return combination;
 }
-
-// export function taskFirst() {
-//   const task = 'I prefer const when I can.';
-//   return task;
-// }
-
-// export function getLast() {
-//   return ' is okay';
-// }
-
-// export function taskNext() {
-//   let combination = 'But sometimes let';
-//   combination += getLast();
-
-//   return combination;
-// }
